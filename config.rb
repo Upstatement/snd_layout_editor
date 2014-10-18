8a26a66a64f239933cce17f62e46987f3bbe8b04
Tito Bottitta <tito@upstatement.com>
2014-10-18 13:58:12 -0400

    * Seeing basic drawer on page * Rough initial CSS * Added auto-prefixer to compass process

--- config.rb
+++ config.rb
@@ -9,3 +9,20 @@ line_comments = true
 # output_style = :compressed
 
 add_import_path 'bower_components/Upbase/components'
+
+# - - - - - - - - - - - - -
+
+# Autoprefixer
+
+# - - - - - - - - - - - - -
+
+# Add Autoprefixer Support
+# https://github.com/ai/autoprefixer#usage
+require 'autoprefixer-rails'
+
+on_stylesheet_saved do |file|
+  css = File.read(file)
+  File.open(file, 'w') do |io|
+    io << AutoprefixerRails.process(css)
+  end
+end
