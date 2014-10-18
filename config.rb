2276fb3ebadba78ba468f48b4115e5c3e22d21c2
Tito Bottitta <tito@upstatement.com>
2014-10-18 11:31:11 -0400

    Adding basic file structure and tooling. Bower. Compass. Etc.

---
+++ config.rb
@@ -0,0 +1,11 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'sass'
+images_dir = 'images'
+javascripts_dir = 'js'
+relative_assets = true
+line_comments = true
+# output_style = :compressed
+
+add_import_path 'bower_components/Upbase/components'
